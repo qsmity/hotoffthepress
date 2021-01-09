7e45c8b142911ebdfb2c4380d119bd81021928cb
qsmit <qsmity@gmail.com>
2021-01-09 10:50:25 -0500

    init commit - server & mongoose setup

---
+++ server/config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    environment: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 8080,
+    dbURI: 'mongodb+srv://hotoffthepress-user:dLZo5chm1fGXCI6U@hotoffthepress.fsllf.mongodb.net/hotoffthepress?retryWrites=true&w=majority'
+}
