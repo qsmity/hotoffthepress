183b55722ee339463189bcb12bd5ba5ab7c862b9
qsmit <qsmity@gmail.com>
2021-01-09 17:28:33 -0500

    created sandbox route to test mongodb - user table

--- server/dist/routes/users.js
+++ server/dist/routes/users.js
@@ -1,8 +1,22 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const user_1 = __importDefault(require("../models/user"));
 const router = express_1.Router();
+// sandbox route to test mongodb
 router.get('/', (req, res) => {
-    res.send('you receieved a user');
+    const user = new user_1.default({
+        username: 'demoUser1',
+        email: 'demo@example.com',
+        password: 'password'
+    });
+    user.save()
+        .then(result => {
+        res.status(201).send(result);
+    })
+        .catch(err => console.log(err));
 });
 exports.default = router;
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express'
+import User from '../models/user'
 
 const router = Router()
 
+// sandbox route to test mongodb
 router.get('/', (req, res) => {
-    res.send('you receieved a user')
+    const user = new User({
+        username: 'demoUser1',
+        email: 'demo@example.com',
+        password: 'password'
+    })
+
+    user.save()
+        .then( result => {
+            res.status(201).send(result)
+        })
+        .catch( err => console.log(err))
+
 })
 
 export default router;
