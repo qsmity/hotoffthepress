ba5432806fe577d677c094920a0d1fd45acb9df8
qsmit <qsmity@gmail.com>
2021-01-10 20:09:30 -0500

    added session reducer

--- client/src/store/reducers/session.tsx
+++ client/src/store/reducers/session.tsx
@@ -1,21 +1,51 @@
 //ACTION TYPES
 const ADD_CURRENT_USER = 'ADD_CURRENT_USER'
-const REMOVE_CURRENT_USER = 'REMOVE_CURRENT_USER'
+/*  
+    don't need 'remove current user' action since logout will just pass an empty user obj
+    which will cause 'isAuthenticated' boolean to be false
+    const REMOVE_CURRENT_USER = 'REMOVE_CURRENT_USER' 
+*/
 
 // TYPES AND INTERFACES
+interface User {
+    id: string;
+    username: string;
+}
+
+interface State {
+    isAuthenticated: boolean;
+    user: User | {}
+}
+
+type Actions = {
+    type: "ADD_CURRENT_USER";
+    user: User
+}
 
 // ACTION CREATORS
+const addCurrentUser = (user: User) => {
+    return {
+        type: ADD_CURRENT_USER,
+        user
+    }
+}
 
 // THUNKS
 
 // REDUCER
 
 const DEFAULT_STATE = {
-    
+    isAuthenticated: false,
+    user: {}
 }
-export default (state = {}, action) => {
+
+export default (state: State = DEFAULT_STATE, action: Actions) => {
     switch (action.type) {
         case ADD_CURRENT_USER:
+            return {
+                isAuthenticated: !!Object.keys(action.user).length, 
+                user: action.user
+            }
         default:
             return state;
     }
