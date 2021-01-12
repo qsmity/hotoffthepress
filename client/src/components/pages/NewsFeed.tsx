a67b6bcf60853e1407f059c7d3ff912a9078e9f9
qsmit <qsmity@gmail.com>
2021-01-12 15:16:18 -0500

    created grid for feed

--- client/src/components/pages/NewsFeed.tsx
+++ client/src/components/pages/NewsFeed.tsx
@@ -1,16 +1,58 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
+import hot3 from '../../hot-3.jpg'
+
 
 const NewsFeed: React.FC<INewsFeedProps> = ({ category }) => {
     return (
         <>
-            <h1>{category}</h1>
-            <h1>{category}</h1>
-            <h1>{category}</h1>
-            <h1>{category}</h1>
-            <h1>{category}</h1>
-            <div className='heading-secondary'>
-                <span className={category}>{category}</span>
-            </div>
+            <header className={`header ${category}`}>
+                <div className='heading-secondary'>
+                    <span className={category}>{category}</span>
+                </div>
+            </header>
+            <main>
+                <div className='refresh'>
+                    <a href='#' className='btn btn--grey'>
+                        update
+                    </a>
+
+                    
+                </div>
+                <section className='section-newsfeed'>
+                    <figure className='section-newsfeed__shape'>
+                        <img className='section-newsfeed__image' src={hot3} alt='' />
+                        <figcaption className='section-newsfeed__text'>
+                            lorem ipsum jidjifj idfjidjfid jidjfidfj
+                        </figcaption>
+                    </figure>
+                    <figure className='section-newsfeed__shape'>
+                        <img className='section-newsfeed__image' src={hot3} alt='' />
+                        <figcaption className='section-newsfeed__text'>
+                            lorem ipsum jidjifj idfjidjfid jidjfidfj
+                        </figcaption>
+                    </figure>
+                    <figure className='section-newsfeed__shape'>
+                        <img className='section-newsfeed__image' src={hot3} alt='' />
+                        <figcaption className='section-newsfeed__text'>
+                            lorem ipsum jidjifj idfjidjfid jidjfidfj
+                        </figcaption>
+                    </figure>
+                    <figure className='section-newsfeed__shape'>
+                        <img className='section-newsfeed__image' src={hot3} alt='' />
+                        <figcaption className='section-newsfeed__text'>
+                            lorem ipsum jidjifj idfjidjfid jidjfidfj
+                        </figcaption>
+                    </figure>
+                    <figure className='section-newsfeed__shape'>
+                        <img className='section-newsfeed__image' src={hot3} alt='' />
+                        <figcaption className='section-newsfeed__text'>
+                            lorem ipsum jidjifj idfjidjfid jidjfidfj
+                        </figcaption>
+                    </figure>
+                </section>
+
+            </main>
         </>
     )
 }
